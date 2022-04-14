00ae0926f0c34b6860f5545ef374803b04920510
Danimlisto16 <50421224+Danimlisto16@users.noreply.github.com>
2022-04-13 22:31:15 -0500

    Initial commit
    Sh file uploaded successfully

---
+++ programs.sh
@@ -0,0 +1,35 @@
+#install notion
+sudo apt update
+sudo apt install snapd
+sudo snap install notion-snap
+
+#install chrome
+sudo apt install wget
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+#install vscode
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
+sudo apt install code
+
+#install wps office
+wget https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/10702/wps-office_11.1.0.10702.XA_amd64.deb
+sudo dpkg -i wps-office_11.1.0.9126.XA_amd64.deb
+
+#install spotify
+sudo apt install curl
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+
+
+#install mailspring
+sudo snap install mailspring
+
+#install vlc
+sudo apt install vlc 
+#chrome gnome shell
+sudo apt-get install chrome-gnome-shell
+
