8efb49ffbf4340c47038514d5e42d595124af3dc
Danimlisto16 <50421224+Danimlisto16@users.noreply.github.com>
2023-04-26 12:27:41 -0500

    Update programs.sh

--- programs.sh
+++ programs.sh
@@ -40,13 +40,9 @@ sudo apt -y install unrar
 #install howdoi - search in stackoverflow
 pip install howdoi
 
-#install github CLI
-type -p curl >/dev/null || sudo apt install curl -y
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& sudo apt update \
-&& sudo apt install gh -y
+#install git
+sudo snap install git-ubuntu --classic
+
 
 #wireguard alt to any VPN
 sudo apt -y install wireguard
